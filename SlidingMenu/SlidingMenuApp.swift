dfcd922abedea070fac733c5a342032c5aa3d991
Eric Fletcher <insitefule@gmail.com>
2024-02-18 09:12:02 -0500

    Initial Commit

---
+++ SlidingMenu/SlidingMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingMenuApp.swift
+//  SlidingMenu
+//
+//  Created by Eric Fletcher on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SlidingMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
